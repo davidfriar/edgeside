f91c61b4b7f9bace977d731ff003e7a72e2b4ef5
David Friar <david.friar@cognifide.com>
2020-12-01 17:22:23 +0000

    add some logging to debug template

--- src/elements/template-element.ts
+++ src/elements/template-element.ts
@@ -1,6 +1,7 @@
 import { Context } from '../context'
 import Mustache from 'mustache'
 import { BaseElementHandler, ContextReader } from './base-element'
+import { debug } from '../util'
 
 export class TemplateElementHandler extends BaseElementHandler {
   input!: ContextReader
@@ -21,6 +22,8 @@ export class TemplateElementHandler extends BaseElementHandler {
     text.remove()
     if (text.lastInTextNode) {
       const json = await this.input.getJSON()
+      debug('about to render template', json, JSON.stringify(json), this.template)
+
       text.after(Mustache.render(this.template, json), { html: true })
     }
   }
